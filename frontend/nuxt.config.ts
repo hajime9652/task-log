b5c17f29a0b6ac08e394cd106aed04a9ab018975
Hajime Hatano <hajime9652@gmail.com>
2022-12-27 17:21:08 +0900

    fix bugs caused by version up

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,8 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@pinia/nuxt',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt'
   ],
   colorMode: {
     preference: 'system', // default theme
@@ -12,11 +12,11 @@ export default defineNuxtConfig({
   build: {
     transpile: ['@heroicons/vue']
   },
-  serverMiddleware: [
-    { path: '/api/auth', handler: '~/server/api/auth.ts' },
-    { path: '/api/auth/**', handler: '~/server/api/auth.ts' },
-    { path: '/api/projects', handler: '~/server/api/projects.ts' },
-    { path: '/api/projects/**', handler: '~/server/api/projects.ts' },
+  serverHandlers: [
+    { route: '/api/auth', handler: '~/server/api/auth.ts' },
+    { route: '/api/auth/**', handler: '~/server/api/auth.ts' },
+    { route: '/api/projects', handler: '~/server/api/projects.ts' },
+    { route: '/api/projects/**', handler: '~/server/api/projects.ts' },
   ],
 })
 
